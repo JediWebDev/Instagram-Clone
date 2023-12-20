36fb276724a0363a7d40cbab49e5fa4e3b48b93c
Jedi-Web-Dev <andrew.soria.ca@gmail.com>
2023-12-19 18:17:02 -0800

    Fix import statement error

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
-import Navbar from "../../components/Navbar/Navbar"; 
+import Navbar from "../../components/NavBar/Navbar"; 
 
 // instead of adding the Sidebar component to every page, we can add it only once to the PageLayout component and wrap the children with it. This way, we can have a sidebar on every page except the AuthPage.
 
